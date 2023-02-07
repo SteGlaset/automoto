e9fb8b78dfff8bd57bce8d0f3d274e2dbf0a04e2
steglaset <steglaset@gmail.com>
2023-02-07 17:31:14 +0300

    feat: add styles for first screen, categories, branches, auto stages and why we sections

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 //webpack.config.js
 const path = require('path');
-const glob = require("glob");
 const PugPlugin = require('pug-plugin');
-const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
 
 const PATHS = {
     src: path.join(__dirname, "src"),
@@ -13,7 +11,7 @@ module.exports = {
         index: './src/index.pug',
     },
     output: {
-        path: path.join(__dirname, 'dist/'),
+        path: path.join(__dirname, 'dist'),
         filename: 'js/[name].[contenthash:8].js'
     },
     plugins: [
@@ -22,11 +20,9 @@ module.exports = {
             css: {
                 filename: 'css/[name].[contenthash:8].css'
             }
-        }),
-        new PurgeCSSPlugin({
-            paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
         })
     ],
+    mode: 'development',
     module: {
         rules: [
             {
@@ -72,17 +68,7 @@ module.exports = {
     devServer: {
         static: {
             directory: path.join(__dirname, 'dist')
-        },
-        watchFiles: {
-            paths: ['src/**/*.*', 'src/styles/**/*.*'],
-            options: {
-                usePolling: true
-            }
         }
     },
-    watchOptions: {
-        poll: 1000, // Check for changes every second
-    },
-    stats: 'errors-only',
-    watch: true
+    stats: 'errors-only'
 };
