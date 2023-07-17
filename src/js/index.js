4587ba270a2835d9875422c02beb314b2348a07d
SteGlaset <90640042+SteGlaset@users.noreply.github.com>
2023-07-17 16:11:58 +0300

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,29 @@
-window.onload = () => {
-    const navLinks = document.querySelectorAll("a.nav-link");
-    const sections = [...navLinks].map(link => {
-        return document.querySelector('#' + link.getAttribute("href").split('#').at(-1))
-    });
-    window.addEventListener("scroll", navHighlighter);
-    function navHighlighter () {
-        let scrollY = window.scrollY;
+const navLinks = document.querySelectorAll("a.anchor-link");
+console.log(navLinks)
+const sections = [...navLinks].map(link => {
+    return document.querySelector('#' + link.getAttribute("href").split('#').at(-1))
+});
+window.addEventListener("scroll", navHighlighter);
+function navHighlighter () {
+    let scrollY = window.scrollY;
 
-        sections.forEach(section => {
-            const sectionHeight = section.offsetHeight;
-            const sectionTop = section.offsetTop - 116;
-            const sectionId = section.getAttribute("id");
-            if (
-                scrollY > sectionTop &&
-                scrollY <= sectionTop + sectionHeight
-            ) {
-                document.querySelector(".modal a.nav-link[href*=" + sectionId + "]").classList.add("active");
-                document.querySelector(".navbar a.nav-link[href*=" + sectionId + "]").classList.add("active");
-            } else {
-                document.querySelector(".modal a.nav-link[href*=" + sectionId + "]").classList.remove("active");
-                document.querySelector(".navbar a.nav-link[href*=" + sectionId + "]").classList.remove("active");
-            }
-        });
-    }
+    sections.forEach(section => {
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 116;
+        const sectionId = section.getAttribute("id");
+        if (
+            scrollY > sectionTop &&
+            scrollY <= sectionTop + sectionHeight
+        ) {
+            document.querySelector(".modal a.nav-link[href*=" + sectionId + "]").classList.add("active");
+            document.querySelector(".navbar a.nav-link[href*=" + sectionId + "]").classList.add("active");
+        } else {
+            document.querySelector(".modal a.nav-link[href*=" + sectionId + "]").classList.remove("active");
+            document.querySelector(".navbar a.nav-link[href*=" + sectionId + "]").classList.remove("active");
+        }
+    });
 }
+
 
 
 
